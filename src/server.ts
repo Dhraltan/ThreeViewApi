ccb46d4d824b9a0e5246184a8d0b074bed2ab5a2
Gheorghe Catalin <catalin.gheorghe@cst.ro>
2021-02-17 18:47:21 +0200

    Installed node-postgres

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,33 @@
 import express from "express";
+import bodyParser from 'body-parser';
+// import pool from './dbconfig/dbconnector';
+
+class Server{
+
+    private app;
+
+    constructor() {
+        this.app = express();
+        // this.dbConnect();
+    }
+
+    // private dbConnect() {
+    //     pool.connect(function (err, client, done) {
+    //         if (err) throw new Error(err);
+    //         console.log('Connected');
+    //       }); 
+    // }
+
+    public start = (port: number) => {
+        return new Promise((resolve, reject) => {
+            this.app.listen(port, () => {
+                resolve(port);
+            }).on('error', (err: Object) => reject(err));
+        });
+    }
+}
+
+
 const app = express();
 const port = 8080; // default port to listen
 
