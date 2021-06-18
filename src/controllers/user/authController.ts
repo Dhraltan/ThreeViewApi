85db50e1eef42e33eb688a5fb1841232619d1d8e
Gheorghe Catalin <catalin.gheorghe@cst.ro>
2021-06-18 18:07:54 +0300

    Implemented change password functionality

--- src/controllers/user/authController.ts
+++ src/controllers/user/authController.ts
@@ -1,39 +1,58 @@
-import { User } from "../../entities/User";
-import { getRepository } from "typeorm";
-import { NextFunction, Request, Response } from "express";
+import {User} from "../../entities/User";
+import {getRepository} from "typeorm";
+import {NextFunction, Request, Response} from "express";
 import bcrypt from "bcrypt";
 import HttpException from "../../middlewares/exceptions/HttpException";
-import { UserDTO } from "../../interfaces/DTOs/UserDTO";
-import { LoginDTO } from "../../interfaces/DTOs/LoginDTO";
-import { jwtToken } from "../../utils/jwtToken.util";
+import {UserDTO} from "../../interfaces/DTOs/UserDTO";
+import {LoginDTO} from "../../interfaces/DTOs/LoginDTO";
+import {jwtToken} from "../../utils/jwtToken.util";
+import {ChangePasswordDTO} from "../../interfaces/DTOs/ChangePasswordDTO";
 
 export class AuthController {
   async login(request: Request, response: Response, next: NextFunction) {
     const userRepository = getRepository(User);
     const loginData: LoginDTO = request.body;
     await userRepository
-      .findOneOrFail({ where: { email: request.body.email } })
+      .findOneOrFail({where: {email: request.body.email}})
       .then(async (user) => {
-        const isPasswordMatching = await bcrypt.compare(
-          loginData.password,
-          user.password
-        );
+        const isPasswordMatching = await bcrypt.compare(loginData.password, user.password);
         if (isPasswordMatching) {
           user.password = null;
           const tokenData = jwtToken.createToken(user);
           response.setHeader("Set-Cookie", [jwtToken.createCookie(tokenData)]);
           return response.send(user);
         } else {
-          return request.next(
-            new HttpException(401, "Password doesn't match!")
-          );
+          return request.next(new HttpException(401, "Password doesn't match!"));
         }
       })
       .catch((err) => {
         if ((err.name = "EntityNotFound")) {
-          return request.next(
-            new HttpException(401, "The entity was not found!")
-          );
+          return request.next(new HttpException(401, "The entity was not found!"));
+        } else {
+          return request.next(new HttpException(400, "Bad Request"));
+        }
+      });
+  }
+
+  async changePassword(request: Request, response: Response, next: NextFunction) {
+    const userRepository = getRepository(User);
+    const bodyData: ChangePasswordDTO = request.body;
+    await userRepository
+      .findOneOrFail({where: {email: request.body.email}})
+      .then(async (user) => {
+        const isPasswordMatching = await bcrypt.compare(bodyData.oldPassword, user.password);
+        if (isPasswordMatching) {
+          const hashedPassword = await bcrypt.hash(bodyData.newPassword, 10);
+          userRepository.update({id: user.id}, {password: hashedPassword});
+
+          return response.sendStatus(204);
+        } else {
+          return request.next(new HttpException(401, "Old password doesn't match!"));
+        }
+      })
+      .catch((err) => {
+        if ((err.name = "EntityNotFound")) {
+          return request.next(new HttpException(401, "The entity was not found!"));
         } else {
           return request.next(new HttpException(400, "Bad Request"));
         }
@@ -56,17 +75,11 @@ export class AuthController {
       .catch((err) => {
         if (err.code == 23502) {
           return request.next(
-            new HttpException(
-              400,
-              `Integrity Constraint Violation: Property ${err.column} is null`
-            )
+            new HttpException(400, `Integrity Constraint Violation: Property ${err.column} is null`)
           );
         } else if (err.code == 23505) {
           return request.next(
-            new HttpException(
-              400,
-              `Integrity Constraint Violation: Email already exists`
-            )
+            new HttpException(400, `Integrity Constraint Violation: Email already exists`)
           );
         } else {
           return request.next(new HttpException(500, err.detail));
--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -6,6 +6,7 @@ const path = "/api/auth";
 const authController = new AuthController();
 
 authRouter.post(`${path}/register`, authController.register);
+authRouter.post(`${path}/changePassword`, authController.changePassword);
 authRouter.post(`${path}/login`, authController.login);
 authRouter.get(`${path}/logout`, authController.logout);
 
