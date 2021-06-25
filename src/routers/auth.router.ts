c1fb9afc76ae18c6c497f000562e595ad6927a95
Gheorghe Catalin <catalin.gheorghe@cst.ro>
2021-06-25 18:27:21 +0300

    Added tokenMiddleware

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -1,12 +1,13 @@
 import express from "express";
+import tokenMiddleware from "../middlewares/tokenAuth/token.middleware";
 import { AuthController } from "../controllers/user/authController";
 
 const authRouter = express.Router();
 const path = "/api/auth";
 const authController = new AuthController();
 
-authRouter.post(`${path}/register`, authController.register);
-authRouter.post(`${path}/changePassword`, authController.changePassword);
+authRouter.post(`${path}/register`, tokenMiddleware, authController.register);
+authRouter.post(`${path}/changePassword`, tokenMiddleware, authController.changePassword);
 authRouter.post(`${path}/login`, authController.login);
 authRouter.get(`${path}/logout`, authController.logout);
 
--- src/routers/elastic.router.ts
+++ src/routers/elastic.router.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import tokenMiddleware from "../middlewares/tokenAuth/token.middleware";
 import { ElasticController } from "../controllers/elastic/ElasticController";
 
 const elasticRouter = express.Router();
 const path = "/api/elastic";
 const contactController = new ElasticController();
 
-elasticRouter.post(`${path}`, contactController.getIndexInfo);
+elasticRouter.post(`${path}`, tokenMiddleware, contactController.getIndexInfo);
 
 export { elasticRouter };
